0f400ac4d8ecf5163b4db3efcf9522a1209e553e
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-12 19:34:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |names, index|
+  index += 1
+  puts "#{index} #{names}"
+  end  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |yells|
+    "#{yells.capitalize!}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+calls.any? { |yells| yells.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arrstrings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+#arrstrings.include?(cheese_types) do |ingreds|
+#return "#{ingreds}"
+ 
+arrstrings.find { |ingreds| cheese_types.include?(ingreds) }
+ 
 end
